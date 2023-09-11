b4a20c1a7ccae03b0aefdfa7918271333caf8b86
Anish Tamse <anish@fasterai.io>
2023-09-11 18:52:42 +0800

    Change to black; fetch price placeholder

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -8,6 +8,7 @@ import { useState, useEffect } from "react";
 import {  SwapOutlined  } from "@ant-design/icons";
 
 const provider = new Provider(Network.LOCAL);
+const provider2 = new Provider(Network.MAINNET);
 // change this to be your module account address
 const coin_owner="0xf1f73e02b4db78e95559caa10a3450dd06e19d55f2036f62773fa7f0617b504f";
 const liquidswap="0xee2610bcc5e690d166ce6c4b7a78c93afca1ebbe5ace4f89baf379fe79e513bb";
@@ -24,6 +25,7 @@ function App() {
   const [balanceUsd, setBalanceUsd] = useState(0);
   const [inputAmount, setInputAmount] = useState(0);
   const [swapOrder, setSwapOrder] = useState([COINS.USD, COINS.ISC]);
+  const [price, setPrice] = useState(0);
 
   const coinToString = (coin) => {
     switch (coin) {
@@ -70,9 +72,25 @@ function App() {
     }
   };
 
+  const fetchPrice = async () => {
+    if (!account) return [];
+    try {
+      let resource = `0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>`;
+      const CoinStore = await provider.getAccountResource(
+        account.address,
+        resource
+      );
+      let balance = CoinStore.data.coin.value/1000000;
+      if (coin==COINS.APT) setBalance(coin, balance/100);
+      else setBalance(coin, balance);
+    } catch (e) {
+    }
+  };
+
   useEffect(() => { fetchBalance(COINS.APT); }, [account?.address]);
   useEffect(() => { fetchBalance(COINS.ISC); }, [account?.address]);
   useEffect(() => { fetchBalance(COINS.USD); }, [account?.address]);
+  useEffect(() => { fetchPrice(); }, [account?.address]);
 
   const setBalance = (coin, value) => {
     switch (coin) {
@@ -119,24 +137,27 @@ function App() {
   };
 
   return (
-    <>
+    <div style= {{backgroundColor: '#000000', height: '1000000px'}}>
       <Layout>
-        <Row align="middle">
+        <Row align="middle" style={{backgroundColor: '#222222'}}>
           <Col span={10} offset={2}>
-            <h1>FxSwap</h1>
+            <h1 style={{color: '#dddddd'}}>FxSwap</h1>
           </Col>
-          <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
+          <Col span={12} style={{ textAlign: "right", paddingRight: "100px" }}>
             <WalletSelector />
           </Col>
         </Row>
       </Layout>
-      <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+      <Row gutter={[0, 32]} style={{ marginTop: "2rem"}}>
         <Col span={6} offset={10}>
           <Card title="Stable Swap">
             <Input suffix={coinToString(swapOrder[0])} onChange={updateAmount}/>
-            <br />
-            <Button shape="circle" size="small" icon={<SwapOutlined rotate="90" onClick={changeSwapOrder}/>} flex="center"/>
-            <br />
+            <div style={{'display': 'flex', 'flexDirection': 'column',
+                          'alignItems': 'center',
+                          'textAlign:': 'center'
+            }}>
+              <Button shape="circle" size="small" icon={<SwapOutlined rotate="90" onClick={changeSwapOrder}/>} flex="center"/>
+            </div>
             <Input suffix={coinToString(swapOrder[1])} />
             <br />
             <br />
@@ -147,7 +168,7 @@ function App() {
           </Card>
         </Col>
       </Row>
-    </>
+    </div>
   );
 }
 
