86b6126a4d1a028842c80510c1992a5cfd409d50
Anish Tamse <anish@fasterai.io>
2023-09-11 16:42:55 +0800

    Make the coin ordering general; Read the input values and make the order change button functional

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import { Layout, Row, Col, Button, Card, Input } from "antd";
+import { Layout, Row, Col, FloatButton, Button, Card, Input } from "antd";
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 import { Provider, Network } from "aptos";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { useState, useEffect } from "react";
+import {  SwapOutlined  } from "@ant-design/icons";
 
 const provider = new Provider(Network.LOCAL);
 // change this to be your module account address
 const coin_owner="0xf1f73e02b4db78e95559caa10a3450dd06e19d55f2036f62773fa7f0617b504f";
 const liquidswap="0xee2610bcc5e690d166ce6c4b7a78c93afca1ebbe5ace4f89baf379fe79e513bb";
+const COINS={
+  "APT": 0,
+  "ISC": 1,
+  "USD": 2,
+}
 
 function App() {
   const { account, signAndSubmitTransaction } = useWallet();
@@ -17,31 +23,77 @@ function App() {
   const [balanceIsc, setBalanceIsc] = useState(0);
   const [balanceUsd, setBalanceUsd] = useState(0);
   const [inputAmount, setInputAmount] = useState(0);
+  const [swapOrder, setSwapOrder] = useState([COINS.USD, COINS.ISC]);
 
-  useEffect(() => { fetchBalance('APT'); }, [account?.address]);
-  useEffect(() => { fetchBalance('ISC'); }, [account?.address]);
-  useEffect(() => { fetchBalance('USD'); }, [account?.address]);
+  const coinToString = (coin) => {
+    switch (coin) {
+      case COINS.APT:
+        return "APT";
+      case COINS.ISC:
+        return "ISC";
+      case COINS.USD:
+        return "USD";
+    }
+  }
+
+  const getResourceAddress = (coin) => {
+    switch (coin) {
+      case COINS.APT:
+        return `0x1::aptos_coin::AptosCoin`;
+      case COINS.ISC:
+        return `${coin_owner}::isc_coin::IscCoin`;
+      case COINS.USD:
+        return `${coin_owner}::usd_coin::UsdCoin`;
+    }
+  }
 
   const fetchBalance = async (coin) => {
     if (!account) return [];
     try {
       let resource = `0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>`;
-      if (coin=='ISC') {resource = `0x1::coin::CoinStore<${coin_owner}::isc_coin::IscCoin>`}
-      if (coin=='USD') {resource = `0x1::coin::CoinStore<${coin_owner}::usd_coin::UsdCoin>`}
+      switch (coin) {
+        case COINS.APT:
+          resource = `0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>`
+        case COINS.ISC:
+          resource = `0x1::coin::CoinStore<${coin_owner}::isc_coin::IscCoin>`
+        case COINS.USD:
+          resource = `0x1::coin::CoinStore<${coin_owner}::usd_coin::UsdCoin>`
+      }
       const CoinStore = await provider.getAccountResource(
         account.address,
         resource
       );
       let balance = CoinStore.data.coin.value/1000000;
-      if (coin=='APT') setBalanceApt(balance/100);
-      if (coin=='ISC') setBalanceIsc(balance);
-      if (coin=='USD') setBalanceUsd(balance);
+      if (coin==COINS.APT) setBalance(coin, balance/100);
+      else setBalance(coin, balance);
     } catch (e) {
     }
   };
 
+  useEffect(() => { fetchBalance(COINS.APT); }, [account?.address]);
+  useEffect(() => { fetchBalance(COINS.ISC); }, [account?.address]);
+  useEffect(() => { fetchBalance(COINS.USD); }, [account?.address]);
+
+  const setBalance = (coin, value) => {
+    switch (coin) {
+      case COINS.APT:
+        setBalanceApt(value);
+        break;
+      case COINS.ISC:
+        setBalanceIsc(value);
+        break;
+      case COINS.USD:
+        setBalanceUsd(value);
+        break;
+    }
+  }
+
   const updateAmount = async (e) => {
     setInputAmount(e.target.value);
+  };
+
+  const changeSwapOrder = async (e) => {
+    setSwapOrder([swapOrder[1], swapOrder[0]])
   };
 
   const performSwap = async () => {
@@ -51,8 +103,8 @@ function App() {
       type: "entry_function_payload",
       function: `${liquidswap}::scripts::swap`,
       type_arguments: [
-        `${coin_owner}::usd_coin::UsdCoin`,
-        `${coin_owner}::isc_coin::IscCoin`,
+        getResourceAddress(swapOrder[0]),
+        getResourceAddress(swapOrder[1]),
         `${liquidswap}::curves::Stable`,
       ],
       arguments: [parseInt(inputAmount)*1000000, 5],
@@ -79,12 +131,13 @@ function App() {
         </Row>
       </Layout>
       <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
-        <Col span={4} offset={10}>
+        <Col span={6} offset={10}>
           <Card title="Stable Swap">
-            <Input prefix="I" suffix="ISC" onChange={updateAmount}/>
+            <Input suffix={coinToString(swapOrder[0])} onChange={updateAmount}/>
             <br />
+            <Button shape="circle" size="small" icon={<SwapOutlined rotate="90" onClick={changeSwapOrder}/>} flex="center"/>
             <br />
-            <Input prefix="$" suffix="USD" />
+            <Input suffix={coinToString(swapOrder[1])} />
             <br />
             <br />
             <Button onClick={performSwap} block type="primary" /*style={{ height: "40px", backgroundColor: "#3f67ff" }}*/>
